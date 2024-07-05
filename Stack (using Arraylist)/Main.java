4368d0b75f68d5c91ee0103b09038032b17d0a84
Tanmay Limje <103094622+TanmayLimje@users.noreply.github.com>
2024-07-05 23:34:48 +0530

    Update Main.java

--- Stack (using Arraylist)/Main.java
+++ Stack (using Arraylist)/Main.java
@@ -1,8 +1,25 @@
 package Stack;
 
 public class Main {
-  
-        Stack_arraylist MyStack = new Stack_arraylist();
+
+    public static String reverseString(String string) {
+        Stack_arraylist<Character> stack = new Stack_arraylist<Character>();
+        String reversedString = "";
+
+        for (char c : string.toCharArray()) {
+            stack.push(c);
+        }
+
+        while (!stack.isEmpty()) {
+            reversedString += stack.pop();
+        }
+
+        return reversedString;
+    }
+
+    public static void main(String[] args) {
+
+        Stack_arraylist myStack = new Stack_arraylist();
         myStack.push(1);
         myStack.push(2);
         myStack.push(3);
@@ -15,5 +32,12 @@ public class Main {
 
         System.out.println("\nStack after pop():");
         myStack.printStack();
+
+        System.out.println("\n## reversed string ##");
+
+        String myString = "hello";
+        System.out.println("string:" + myString);
+        String reversedString = reverseString(myString);
+        System.out.println("reversed string: "+reversedString);
     }
 }

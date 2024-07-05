de65a78c0ae9ee34ff1649888552d556388b45dc
Tanmay Limje <103094622+TanmayLimje@users.noreply.github.com>
2024-07-05 15:09:58 +0530

    Create Stack.java

---
+++ Stack (using LL)/Stack.java
@@ -0,0 +1,51 @@
+package Stack;
+
+public class Stack {
+    private Node top;
+    private int height;
+
+    public Stack(int value){
+        Node newNode = new Node(value);
+        top = newNode;
+        height = 1;
+    }
+
+    public void printStack(){
+        Node temp = top;
+        while(temp != null){
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getTop(){
+        System.out.println("Top element is " + top.value);
+    }
+
+    public void getHeight(){
+        System.out.println("Height is " + height);
+    }
+
+    public void push(int value){
+        Node newNode = new Node(value);
+        if(height == 0){
+            top = newNode;
+        }
+        else{
+            newNode.next = top;
+            top = newNode;
+        }
+        height++;
+    }
+
+    public Node pop(){
+        if(height == 0){
+            return null;
+        }
+        Node temp = top;
+        top = top.next;
+        temp.next = null;
+        height--;
+        return temp;
+    }
+}

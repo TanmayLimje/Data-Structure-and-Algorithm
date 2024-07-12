88d480747934242779f52800fc37bc4ff02b1691
Tanmay Limje <103094622+TanmayLimje@users.noreply.github.com>
2024-07-13 01:27:06 +0530

    Create Node.java

---
+++ Hash_Table/Node.java
@@ -0,0 +1,12 @@
+package Hash_table;
+
+public class Node {
+    String key;
+    Node next;
+    int value;
+
+    public Node(String key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}

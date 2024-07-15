c7758fdedf0e5c119da0cbddb1e0ca6d4217bf46
Tanmay Limje <103094622+TanmayLimje@users.noreply.github.com>
2024-07-15 18:12:50 +0530

    Create Main.java

---
+++ Star_pattern/Main.java
@@ -0,0 +1,41 @@
+package star_pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Star star = new Star();
+
+        star.patternOne();
+        star.beautify();
+
+        star.patternTwo();
+        star.beautify();
+
+        star.patternThree();
+        star.beautify();
+
+        star.patternFour();
+        star.beautify();
+
+        star.patternFive();
+        star.beautify();
+
+        star.patternSix();
+        star.beautify();
+
+        star.patternSeven();
+        star.beautify();
+
+        star.patternEight();
+        star.beautify();
+
+        star.patternNine();
+        star.beautify();
+
+        star.patternTen();
+        star.beautify();
+
+        star.patternEleven();
+        star.beautify();
+
+    }
+}

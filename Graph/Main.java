061c85d2a71c1386e4690acc5267e103fde132c2
Tanmay Limje <103094622+TanmayLimje@users.noreply.github.com>
2024-07-16 08:12:33 +0530

    Create Main.java

---
+++ Graph/Main.java
@@ -0,0 +1,23 @@
+package Graph;
+
+public class Main {
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+        graph.addVertex("A");
+        graph.addVertex("B");
+        graph.addVertex("C");
+        graph.addVertex("D");
+        graph.printGraph();
+
+        graph.addEdge("A", "B");
+        graph.addEdge("A", "C");
+        graph.addEdge("B", "D");
+        graph.addEdge("C", "D");
+        graph.addEdge("D", "A");
+        graph.printGraph();
+
+        graph.removeVertex("D");
+        //graph.removeEdge("A", "C");
+        graph.printGraph();
+    }
+}

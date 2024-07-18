4893487f0759cc15a9f4d39f6e1a028c81e8c2de
Tanmay Limje <103094622+TanmayLimje@users.noreply.github.com>
2024-07-18 19:42:13 +0530

    Create Mosnter.java

---
+++ Moster-Hunter/Mosnter.java
@@ -0,0 +1,11 @@
+package Moster_hunter;
+
+public class Monster {
+    int power;
+    int bonus;
+
+    Monster(int power, int exp) {
+        this.power = power;
+        this.bonus = exp;
+    }
+}

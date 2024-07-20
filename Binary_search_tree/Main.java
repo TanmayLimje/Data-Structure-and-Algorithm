86f2b505e06942f5d3ec3162bd957c562100393c
Tanmay Limje <103094622+TanmayLimje@users.noreply.github.com>
2024-07-20 21:23:43 +0530

    Update Main.java

--- Binary_search_tree/Main.java
+++ Binary_search_tree/Main.java
@@ -1,19 +1,45 @@
-package Binary_Search_Tree;
+package BST;
 
 public class Main {
     public static void main(String[] args) {
         BinarySearchTree bst = new BinarySearchTree();
-        bst.insert(47);
-        bst.insert(21);
-        bst.insert(76);
-        bst.insert(18);
-        bst.insert(27);
-        bst.insert(52);
-        bst.insert(82);
+//        bst.insert(47);
+//        bst.insert(21);
+//        bst.insert(76);
+//        bst.insert(18);
+//        bst.insert(27);
+//        bst.insert(52);
+//        bst.insert(82);
 
-        //System.out.println(bst.root.left.right.right.value);
+        //System.out.println("min value in tree: "+bst.minValue(bst.root));
+//
+//        //System.out.println(bst.root.left.right.right.value);
+//
+//        System.out.println(bst.rContains(27));
+//        System.out.println(bst.rContains(17));
 
-        System.out.println(bst.contains(27));
-        System.out.println(bst.contains(17));
+          bst.rInsert(2);
+          bst.insert(1);
+          bst.insert(3);
+
+        System.out.println("\nRoot: " + bst.root.value);
+        System.out.println("Root->left : " + bst.root.left.value);
+        System.out.println("Root->right : " + bst.root.right.value);
+
+        bst.deleteNode(2);
+
+        System.out.println("\nRoot: " + bst.root.value);
+        System.out.println("Root->left : " + bst.root.left.value);
+        System.out.println("Root->right : " + bst.root.right);
     }
 }
+
+// op:--->>>
+
+//Root: 2
+//Root->left : 1
+//Root->right : 3
+//
+//Root: 3
+//Root->left : 1
+//Root->right : null

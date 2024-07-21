fe48c699a8c547a97e7c93d4e1935ebf601ad82d
Tanmay Limje <103094622+TanmayLimje@users.noreply.github.com>
2024-07-21 18:10:00 +0530

    Update Main.java

--- Binary_search_tree/Main.java
+++ Binary_search_tree/Main.java
@@ -3,13 +3,19 @@ package BST;
 public class Main {
     public static void main(String[] args) {
         BinarySearchTree bst = new BinarySearchTree();
-//        bst.insert(47);
-//        bst.insert(21);
-//        bst.insert(76);
-//        bst.insert(18);
-//        bst.insert(27);
-//        bst.insert(52);
-//        bst.insert(82);
+        bst.insert(47);
+        bst.insert(21);
+        bst.insert(76);
+        bst.insert(18);
+        bst.insert(27);
+        bst.insert(52);
+        bst.insert(82);
+
+        System.out.println("\nTree Traversal:\n");
+        System.out.println("BFS: " + bst.BFS());
+        System.out.println("DFS-PreOrder: " + bst.DFSPreOrder());
+        System.out.println("DFS-PostOrder: " + bst.DFSPostOrder());
+        System.out.println("DFS-InOrder: " + bst.DFSInOrder());
 
         //System.out.println("min value in tree: "+bst.minValue(bst.root));
 //
@@ -18,28 +24,22 @@ public class Main {
 //        System.out.println(bst.rContains(27));
 //        System.out.println(bst.rContains(17));
 
-          bst.rInsert(2);
-          bst.insert(1);
-          bst.insert(3);
-
-        System.out.println("\nRoot: " + bst.root.value);
-        System.out.println("Root->left : " + bst.root.left.value);
-        System.out.println("Root->right : " + bst.root.right.value);
-
-        bst.deleteNode(2);
-
-        System.out.println("\nRoot: " + bst.root.value);
-        System.out.println("Root->left : " + bst.root.left.value);
-        System.out.println("Root->right : " + bst.root.right);
+//          bst.rInsert(2);
+//          bst.insert(1);
+//          bst.insert(3);
+//
+//        System.out.println("\nRoot: " + bst.root.value);
+//        System.out.println("Root->left : " + bst.root.left.value);
+//        System.out.println("Root->right : " + bst.root.right.value);
+//
+//        bst.deleteNode(2);
+//
+//        System.out.println("\nRoot: " + bst.root.value);
+//        System.out.println("Root->left : " + bst.root.left.value);
+//        System.out.println("Root->right : " + bst.root.right);
     }
 }
 
-// op:--->>>
 
-//Root: 2
-//Root->left : 1
-//Root->right : 3
-//
-//Root: 3
-//Root->left : 1
-//Root->right : null
+
+

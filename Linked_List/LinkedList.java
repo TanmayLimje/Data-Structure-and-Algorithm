437e0288b306156fdb8d9e4d08435dcb04bea738
Tanmay Limje <103094622+TanmayLimje@users.noreply.github.com>
2024-07-22 14:39:21 +0530

    Update LinkedList.java

--- Linked_List/LinkedList.java
+++ Linked_List/LinkedList.java
@@ -1,5 +1,8 @@
 package linked_list;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LinkedList {
     private Node head;
     private Node tail;
@@ -62,12 +65,14 @@ public class LinkedList {
         }
     }
 
-    public void getHead() {
-        System.out.println("Head is " + head.value);
+    public Node getHead() {
+        //System.out.println("Head is " + head.value);
+        return head;
     }
 
-    public void getTail() {
-        System.out.println("Tail is " + tail.value);
+    public Node getTail() {
+        //System.out.println("Tail is " + tail.value);
+        return tail;
     }
 
     public void getLength() {
@@ -192,5 +197,83 @@ public class LinkedList {
             fast = fast.next.next;
         }
         return slow;
+    }
+
+    public boolean hasLoop(){
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public Node findKthFromEnd(int k){
+        Node fast = head;
+        Node slow = head;
+
+        for(int i = 0; i < k; i++){
+            if(fast == null)
+                return null;
+            fast = fast.next;
+        }
+
+        while (fast != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow;
+    }
+
+    public void partitionList(int x){
+        if(head == null) return;
+
+        Node dummy1 = new Node(0);
+        Node dummy2 = new Node(0);
+        Node prev1 = dummy1;
+        Node prev2 = dummy2;
+        Node current = head;
+
+        while  (current != null){
+            if(current.value < x){
+                prev1.next = current;
+                prev1 = current;
+            } else {
+                prev2.next = current;
+                prev2 = current;
+            }
+            current = current.next;
+        }
+
+        prev2.next = null;
+        prev1.next = dummy2.next;
+
+        head = dummy1.next;
+    }
+
+    public void removeDuplicates(){
+        Node prev = null;
+        Node current = head;
+        Set<Integer> values = new HashSet<>();
+
+        while (current != null){
+            if(values.contains(current.value)){
+                prev.next = current.next;
+                length -= 1;
+            }
+            else {
+                values.add(current.value);
+                prev = current;
+            }
+            current = current.next;
+        }
     }
 }

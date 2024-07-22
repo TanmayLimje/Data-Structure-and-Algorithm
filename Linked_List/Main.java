81ce6c427be06a815fead9f037516330302d94f1
Tanmay Limje <103094622+TanmayLimje@users.noreply.github.com>
2024-07-22 14:39:39 +0530

    Update Main.java

--- Linked_List/Main.java
+++ Linked_List/Main.java
@@ -3,17 +3,33 @@ package linked_list;
 public class Main {
     public static void main(String[] args) {
 
-        LinkedList myLinkedList = new LinkedList(1);
-        myLinkedList.append(2);
-        myLinkedList.append(3);
-        myLinkedList.append(4);
+        LinkedList myLinkedList = new LinkedList(3);
+        myLinkedList.append(8);
         myLinkedList.append(5);
-        myLinkedList.append(6);
+        myLinkedList.append(10);
+        myLinkedList.append(2);
+        myLinkedList.append(1);
+        myLinkedList.append(5);
         myLinkedList.print();
 
-        Node middle = myLinkedList.findMiddleNode();
-        System.out.println("middle node:" + middle.value);
+        myLinkedList.removeDuplicates();
+        System.out.println("\nLL after removing dupes");
+        myLinkedList.print();
 
+//        myLinkedList.partitionList(5);
+//        System.out.println("\nLL after partiton:");
+//        myLinkedList.print();
+
+//        System.out.println("\nKth value from end: ");
+//        System.out.println(myLinkedList.findKthFromEnd(2).value);
+
+//        myLinkedList.getTail().next = myLinkedList.getHead().next;
+//
+//        System.out.println("Has Loop:");
+//        System.out.println( myLinkedList.hasLoop());
+
+//        Node middle = myLinkedList.findMiddleNode();
+//        System.out.println("middle node:" + middle.value);
 
     }
 }

d85889fbac8b6bc8f485f3033363504396dfacac
Tanmay Limje <103094622+TanmayLimje@users.noreply.github.com>
2024-07-22 16:03:42 +0530

    Update Main.java

--- Doubly_Linked_list/Main.java
+++ Doubly_Linked_list/Main.java
@@ -3,10 +3,24 @@ package Doubly_linked_list;
 public class Main {
     public static void main(String[] args) {
         DoublyLinkedList myDLL = new DoublyLinkedList(1);
-        myDLL.append(1);
+//        myDLL.append(1);
         myDLL.append(2);
+        myDLL.append(3);
+        myDLL.append(4);
 
-        myDLL.remove(1);
+       // myDLL.remove(1);
+        myDLL.printList();
+
+        System.out.println();
+
+//        myDLL.reverse();
+//        myDLL.printList();
+
+//        System.out.println("\nis pallindrom");
+//        System.out.println(myDLL.isPalindrome());
+
+        System.out.println("\nSwap pairs");
+        myDLL.swapPairs();
         myDLL.printList();
     }
 }
